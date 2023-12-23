a5577b1695a110569e299b4561a4cc91a0bca535
Abdelouahab Boumaraf <boumarafabdelouhab5@gmail.com>
2023-12-23 15:47:00 +0100

    Initial commit

--- server/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/userController');
-
-router.post('/login', UserController.login);
-
-module.exports = router;
