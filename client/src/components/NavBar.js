d8cfdbcf86a58e4d5a52a26a45cac42fa3d1b9c7
abdelouhabhub <93412308+abdelouhabhub@users.noreply.github.com>
2023-12-24 14:21:18 +0100

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -44,10 +44,10 @@ const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/abdelouahab-boumaraf/"><img src={navIcon1} alt="" /></a>
-                <a href="https://github.com/abdelouhabhub"><img src={navIcon4} alt="" /></a>
-                <a href="https://web.facebook.com/abdelouahabbmrf/"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/a.b.d_0.u/"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/abdelouahab-boumaraf/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/abdelouhabhub" target="_blank"><img src={navIcon4} alt="" /></a>
+                <a href="https://web.facebook.com/abdelouahabbmrf/" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/a.b.d_0.u/" target="_blank"><img src={navIcon3} alt="" /></a>
               </div>
             </span>
           </Navbar.Collapse>
